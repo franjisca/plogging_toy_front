c1ae36e4d0d7b69f39cac990bb880d72fb4cc257
JIYEON <publicjy324@gmail.com>
2023-06-22 23:53:16 +0900

    like count css 살짝 수정

--- src/mypage/mypage_component/MyPhotoList.tsx
+++ src/mypage/mypage_component/MyPhotoList.tsx
@@ -3,15 +3,12 @@ import styled from "@emotion/styled";
 import React from "react";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Typography } from "@mui/material";
+import { Chip, TextField, Typography } from "@mui/material";
 
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 // const [dpNone, setDpNone] = useState<boolean>(false);
 const CustomIcon = styled(FavoriteIcon)({
-    position: 'absolute',
-    top: '18vh',
-    left: '9vw',
     width: '2vw',
     height: '4vh',
     color: '#F5382E',
@@ -36,18 +33,16 @@ const Customdelete = styled(DeleteIcon)({
     } 
 });
 
-const LikedIcon = styled(CustomIcon)({
-    color: '#F5766F',
-});
-
 const LikeCount  = styled(Typography)({
     position: 'absolute',
-    top: '18.5vh',
-    left: '7.8vw',
-    width: '2vw',
+    top: '18.9vh',
+    left: '7vw',
+    width: '4vw',
     height: '4vh',
-    fontSize: '20px',
     fontFamily: 'Orbit',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
 });
 
 const MyPhotoList = (passed: any) => {
@@ -58,14 +53,11 @@ const MyPhotoList = (passed: any) => {
                 <div className="photo basic_sort">
                 <img className="photo-size" src="/image/free-icon-mother-earth-day-4287570.png"/>
                 <Customdelete />
-                <LikeCount>25</LikeCount>
-                <CustomIcon />
+                <LikeCount>25<CustomIcon/></LikeCount>
                 </div>
                 <div className="photo basic_sort">
                 <img className="photo-size" src="/image/free-icon-mother-earth-day-4287570.png"/>
                 <Customdelete />
-                <LikeCount>25</LikeCount>
-                <LikedIcon />
                 </div>
                
     </div>
