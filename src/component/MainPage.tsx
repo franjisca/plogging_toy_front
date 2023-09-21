80a0f510aa985b634a24de699c03921f3075febc
Jiyeon <96047335+franjisca@users.noreply.github.com>
2023-09-21 13:39:49 +0900

    날짜데이터 수정

--- src/component/MainPage.tsx
+++ src/component/MainPage.tsx
@@ -17,10 +17,16 @@ const MainPage = () => {
 
 
     const [data, setData] = useState<any>('');
+    const date = new Date();
+    const year = date.getFullYear();
+    const mon = date.getMonth()+1;
+    const day = date.getDate();
+
+    
     return <div className="main_contents">
         <img className="move" src="/image/free-icon-mother-earth-day-4287570.png" alt="main_image"/>
         <div className="main-text">
-        안녕하세요, 오늘은 2023년 6월 19일<br/>
+        안녕하세요, 오늘은 {year}년 {mon}월 {day}일<br/>
         총 {data ? data : 0}명의 지구방위대 분들이<br/>
         대략 {data ? data * 2 : 0}kg만큼의 쓰레기로부터<br/>
         지구를 지켰어요
