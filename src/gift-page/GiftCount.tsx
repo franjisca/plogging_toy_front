1159285e4b7d801e2599f8309582879e73128e43
JIYEON <publicjy324@gmail.com>
2023-12-30 10:58:03 +0900

    종량제 봉투 기능 만드는 중

--- src/gift-page/GiftCount.tsx
+++ src/gift-page/GiftCount.tsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 
 
 const GiftCount = () => {
 
+    const [dummyCount, setDummyCount] = useState<any>(100);
+    const [myPlasticCount, setMyPlasticCount] = useState<any>(0);
+
     const onClickPlasticBag = () => {
 
+        if(dummyCount <= 0) {
+            toast.error("현재 남은 종량제봉투가 없습니다. 내일 다시 시도해주세요!");
+            return;
+        }
+
+        setDummyCount(dummyCount-1);
+        setMyPlasticCount(myPlasticCount+1);
     }
 
     return <div className="main_contents">
             <div className="meeting-area">
                 <div className="basic_sort h60vh">
-                    <img className="plastic_bag_img move" src="/image/trash.png" alt="plastic_bag_img"/>
-                    <span className="position-absolute">100개</span>
+                    <img className="plastic_bag_img move" src="/image/trash.png" alt="plastic_bag_img" onClick={onClickPlasticBag}/>
+                    <span className="position-absolute">{dummyCount}개</span>
                 </div>
                     <p className="block_text basic_sort h6vh ">이미지를 클릭하면 봉투를 받을 수 있어요!</p>
                 <div className="round-box">
-                    <span>지금 내가 가지고 있는 종량제 봉투</span>
-                    <div>10개</div>
+                    <span>지금 내가 가지고 있는 종량제 봉투
+                    </span>
+                    <div className="basic_sort"><span>{myPlasticCount}개</span> <button className="button-37">교환하기</button></div>
                  </div>
             </div>       
         </div>
