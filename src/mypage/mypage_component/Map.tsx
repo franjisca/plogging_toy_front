917f101ecb7efc00611ebd6b633b72fca69e65c3
JIYEON <publicjy324@gmail.com>
2024-04-04 12:01:00 +0900

    지도 API 추가

--- src/mypage/mypage_component/Map.tsx
+++ src/mypage/mypage_component/Map.tsx
@@ -12,7 +12,6 @@ const MyMap = (passed:any) => {
       style={{ width: "100%", height: "100%" }}
     >
       <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{color:"#000"}}>Hello World!</div>
       </MapMarker>
     </Map>
     </div>
